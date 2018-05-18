f973a075dec96521029243e8061b075aeadf4322
Maki-Daisuke <maki.daisuke+gh@gmail.com>
2018-05-18 18:21:46 +0900

    Initial import

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.nakka.com/soft/clcl/download/clcl203_eng.zip'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url           = $url
+
+  softwareName  = 'clcl.portable*'
+
+  checksum      = '0538CCFFD580CCE7833D3B4D1BF85F5CE65DB42284688479CC0E671D91A8265F'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
